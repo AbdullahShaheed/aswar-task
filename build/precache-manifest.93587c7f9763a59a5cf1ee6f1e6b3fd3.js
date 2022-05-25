53212c6e6fdd8fd7a348292cd05ceec810076eca
Abd <abdullahzwed@gmail.com>
2022-05-25 18:59:18 +0300

    Second commit

---
+++ build/precache-manifest.93587c7f9763a59a5cf1ee6f1e6b3fd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb4db5412c6853171d87b27ed770806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbd05d7241efd185110e",
+    "url": "/static/css/2.19d6eef7.chunk.css"
+  },
+  {
+    "revision": "0f7392dc5afc39eed144",
+    "url": "/static/css/main.8943ddab.chunk.css"
+  },
+  {
+    "revision": "bbd05d7241efd185110e",
+    "url": "/static/js/2.d7caacf2.chunk.js"
+  },
+  {
+    "revision": "ab8c5cb0e0c2a8172faa67cb90d8079e",
+    "url": "/static/js/2.d7caacf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7392dc5afc39eed144",
+    "url": "/static/js/main.2b50151c.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
